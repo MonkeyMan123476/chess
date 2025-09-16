aa052d01063ff5c2fffba9bd35bf82977be69871
MonkeyMan123476 <csandorf@byu.edu>
2025-09-16 15:52:06 -0600

    Implemented KingMovesCalculator and started on QueenMovesCalculator

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -11,13 +11,15 @@ public class PieceMovesCalculator {
             BishopMovesCalculator Bishop = new BishopMovesCalculator();
             return Bishop.pieceMoves(board, myPosition, piece);
         } else if (piece.getPieceType() == ChessPiece.PieceType.KING) {
-            throw new RuntimeException("Not implemented");
+            KingMovesCalculator King = new KingMovesCalculator();
+            return King.pieceMoves(board, myPosition, piece);
         } else if (piece.getPieceType() == ChessPiece.PieceType.KNIGHT) {
             throw new RuntimeException("Not implemented");
         } else if (piece.getPieceType() == ChessPiece.PieceType.PAWN) {
             throw new RuntimeException("Not implemented");
         } else if (piece.getPieceType() == ChessPiece.PieceType.QUEEN) {
-            throw new RuntimeException("Not implemented");
+            QueenMovesCalculator Queen = new QueenMovesCalculator();
+            return Queen.pieceMoves(board, myPosition, piece);
         } else if (piece.getPieceType() == ChessPiece.PieceType.ROOK) {
             throw new RuntimeException("Not implemented");
         }
@@ -37,11 +39,9 @@ class BishopMovesCalculator extends PieceMovesCalculator {
         List<ChessMove> moveList = new ArrayList<>();
         int[][] diagonalDirections = {{-1, -1}, {-1, 1}, {1, -1}, {1, 1}};
         for (int[] dir: diagonalDirections) {
-            int rowDirection = dir[0];
-            int colDirection = dir[1];
-            for (int i = 1; i < 8 ; i++) {
-                int newX = myPosition.getRow() + (i * rowDirection);
-                int newY = myPosition.getColumn() + (i * colDirection);
+            for (int i = 1; i < 8 ; i++) { // dir[0] is rowDirection, dir[1] is colDirection
+                int newX = myPosition.getRow() + (i * dir[0]);
+                int newY = myPosition.getColumn() + (i * dir[1]);
                 if (newX > 0 && newX <= 8 && newY > 0 && newY <= 8) {
                     ChessPosition newPosition = new ChessPosition(newX, newY);
                     if ((board.getPiece(newPosition) != null)) {
@@ -62,7 +62,26 @@ class BishopMovesCalculator extends PieceMovesCalculator {
 }
 
 class KingMovesCalculator extends PieceMovesCalculator {
-
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition, ChessPiece piece) {
+        List<ChessMove> moveList = new ArrayList<>();
+        int[][] moveDirections = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}};
+        for (int[] dir: moveDirections) { // dir[0] rowDirection, dir[1] colDirection
+            int newX = myPosition.getRow() + dir[0];
+            int newY = myPosition.getColumn() + dir[1];
+            if (newX > 0 && newX <= 8 && newY > 0 && newY <= 8) {
+                ChessPosition newPosition = new ChessPosition(newX, newY);
+                if (board.getPiece(newPosition) != null) {
+                    if (checkDifferentColors(board.getPiece(newPosition), piece)) {
+                        moveList.add(new ChessMove(myPosition, newPosition, null));
+                    }
+                } else {
+                    moveList.add(new ChessMove(myPosition, newPosition, null));
+                }
+            }
+        }
+        return moveList;
+    }
 }
 
 class KnightMovesCalculator extends PieceMovesCalculator {
@@ -74,7 +93,30 @@ class PawnMovesCalculator extends PieceMovesCalculator {
 }
 
 class QueenMovesCalculator extends PieceMovesCalculator {
-
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition, ChessPiece piece) {
+        List<ChessMove> moveList = new ArrayList<>();
+        int[][] moveDirections = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}};
+        for (int[] dir: moveDirections) { // dir[0] rowDirection, dir[1] colDirection
+            for (int i = )
+                int newX = myPosition.getRow() + dir[0];
+                int newY = myPosition.getColumn() + dir[1];
+                if (newX > 0 && newX <= 8 && newY > 0 && newY <= 8) {
+                    ChessPosition newPosition = new ChessPosition(newX, newY);
+                    if (board.getPiece(newPosition) != null) {
+                        if (checkDifferentColors(board.getPiece(newPosition), piece)) {
+                            moveList.add(new ChessMove(myPosition, newPosition, null));
+                        }
+                        break;
+                    } else {
+                        moveList.add(new ChessMove(myPosition, newPosition, null));
+                    }
+                } else {
+                    break;
+                }
+        }
+        return moveList;
+    }
 }
 
 class RookMovesCalculator extends PieceMovesCalculator {
