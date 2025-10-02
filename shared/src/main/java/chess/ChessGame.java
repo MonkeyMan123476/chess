2ce840032d96e1585915280c74527297926a3f3d
MonkeyMan123476 <csandorf@byu.edu>
2025-10-01 22:31:42 -0600

    Fixed canKillKing to get rid of teamColor bug, now it passes all MakeMoveTests!

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -34,6 +35,20 @@ public class ChessGame {
         teamTurn = team;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return teamTurn == chessGame.teamTurn && Objects.equals(myBoard, chessGame.myBoard);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamTurn, myBoard);
+    }
+
     /**
      * Enum identifying the 2 possible teams in a chess game
      */
@@ -56,6 +71,8 @@ public class ChessGame {
         List<ChessMove> validMoveList = new ArrayList<>();
         for (ChessMove move: getBoard().getPiece(startPosition).pieceMoves(getBoard(), startPosition)) {
             if (!willBeInCheck(move)) {
+                System.out.println("yeah this move won't put king in check.");
+                System.out.println(move);
                 validMoveList.add(move);
             }
         }
@@ -70,14 +87,19 @@ public class ChessGame {
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
         if (getBoard().getPiece(move.getStartPosition()) == null) {
+            System.out.println("No piece to move");
             throw new InvalidMoveException("No piece to move");
         } else if (getBoard().getPiece(move.getStartPosition()).getTeamColor() != getTeamTurn()) {
+            System.out.println("wrong team");
             throw new InvalidMoveException("Not this team's turn");
         } else if (validMoves(move.getStartPosition()) != null && validMoves(move.getStartPosition()).contains(move)) {
+            System.out.println("moving piece");
             movePiece(getBoard().getPiece(move.getStartPosition()), move.getStartPosition(), move.getEndPosition(), move.getPromotionPiece());
         } else if (willBeInCheck(move)){
+            System.out.println("king still in check");
             throw new InvalidMoveException("King may not remain in check");
         } else {
+            System.out.println("nah");
             throw new InvalidMoveException("Invalid move");
         }
     }
@@ -89,9 +111,7 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-
-
-        return false;
+        return canKillKing(myBoard, teamColor);
     }
 
     /**
@@ -101,7 +121,7 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        return false;
+        return (canKillKing(myBoard, teamColor) && !anyValidMoves(teamColor));
     }
 
     /**
@@ -112,7 +132,18 @@ public class ChessGame {
      * @return True if the specified team is in stalemate, otherwise false
      */
     public boolean isInStalemate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return !anyValidMoves(teamColor);
+    }
+
+    public boolean anyValidMoves(ChessGame.TeamColor teamColor) {
+        for (int row = 1; row <= 8; row++) {
+            for (int col = 1; col <= 8; col++) {
+                ChessPosition testPosition = new ChessPosition(row, col);
+                ChessPiece testPiece = getBoard().getPiece(testPosition);
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -163,7 +194,7 @@ public class ChessGame {
             testBoard.squares[testMove.getEndPosition().getRow() - 1][testMove.getEndPosition().getColumn() - 1] = testBoard.getPiece(testMove.getStartPosition());
             testBoard.squares[testMove.getStartPosition().getRow() - 1][testMove.getStartPosition().getColumn() - 1] = null;
         }
-        return canKillKing(testBoard, teamTurn);
+        return canKillKing(testBoard, getTeamTurn());
     }
 
     public ChessBoard copyBoard() {
@@ -171,8 +202,8 @@ public class ChessGame {
         for (int row = 1; row <= 8; row++) {
             for (int col = 1; col <= 8; col++) {
                 ChessPosition position = new ChessPosition(row, col);
-                if (myBoard.getPiece(position) != null) {
-                    newBoard.addPiece(position, new ChessPiece(myBoard.getPiece(position).getTeamColor(), (myBoard.getPiece(position).getPieceType())));
+                if (getBoard().getPiece(position) != null) {
+                    newBoard.addPiece(position, new ChessPiece(getBoard().getPiece(position).getTeamColor(), (getBoard().getPiece(position).getPieceType())));
                 }
             }
         }
@@ -184,11 +215,13 @@ public class ChessGame {
             for (int col = 1; col <= 8; col++) {
                 ChessPosition testPosition = new ChessPosition(row, col);
                 ChessPiece testPiece = checkingBoard.getPiece(testPosition);
-                if (testPiece != null) {
+                if (testPiece != null && testPiece.getTeamColor() != teamColor) {
                     Collection<ChessMove> potentialMoves = testPiece.pieceMoves(checkingBoard, testPosition);
                     for (ChessMove potentialMove: potentialMoves) {
+                        System.out.println("potential move: " + potentialMove);
                         ChessPiece killedPiece = checkingBoard.getPiece(potentialMove.getEndPosition());
-                        if (killedPiece != null && killedPiece.getPieceType() == ChessPiece.PieceType.KING && killedPiece.getTeamColor() != teamColor) {
+                        if (killedPiece != null && killedPiece.getPieceType() == ChessPiece.PieceType.KING && killedPiece.getTeamColor() == teamColor) {
+                            System.out.println("this is gonna kill king");
                             return true;
                         }
                     }
