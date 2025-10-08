de56384acca35831c4cd33eed087394eea9b27f6
MonkeyMan123476 <csandorf@byu.edu>
2025-10-08 15:09:47 -0600

    Register test passes, not implemented though.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,10 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
 
 public class Server {
 
@@ -11,11 +15,20 @@ public class Server {
 
 
         server.delete("db", ctx -> ctx.result("{}"));
-        server.post("user", ctx -> ctx.result("{\"username\":\"joe\", \"authToken\":\"xyz\"}"));
+        server.post("user", this::register);
 
 
         // Register your endpoints and exception handlers here.
 
+    }
+
+
+    private void register(Context ctx) {
+        var serializer = new Gson();
+        var req = serializer.fromJson(ctx.body(), Map.class);
+        req.put("authToken", "cow");
+        var res = serializer.toJson(req);
+        ctx.result(res);
     }
 
     public int run(int desiredPort) {
