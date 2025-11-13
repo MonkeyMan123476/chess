fa5d2f7a51e2d04075a271d18d0f8bb99c9f44e7
MonkeyMan123476 <csandorf@byu.edu>
2025-11-13 16:55:12 -0700

    Added row numbers to the drawBoard loops, as well as a white and black function for the row of column labels.

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -44,7 +44,7 @@ public class ChessClient {
 
 
     private void printPrompt() {
-        System.out.print("\n" + EscapeSequences.RESET_TEXT_BLINKING + ">>> " + EscapeSequences.SET_TEXT_COLOR_GREEN);
+        System.out.print("\n" + EscapeSequences.SET_TEXT_COLOR_BLUE + EscapeSequences.RESET_TEXT_BLINKING + ">>> " + EscapeSequences.SET_TEXT_COLOR_GREEN);
     }
 
     public String eval(String input) {
@@ -129,8 +129,8 @@ public class ChessClient {
         try {
             System.out.print("Name your game: ");
             String gameName = scanner.nextLine();
-            var GameData = server.createGame(authToken, gameName);
-            String returnStatement = String.format("You created game %s with the gameID %s.\n", gameName, GameData.gameID());
+            server.createGame(authToken, gameName);
+            String returnStatement = String.format("You created game %s.\n", gameName);
             return returnStatement + help();
         } catch (Exception e) {
             return "Unable to create game. Please choose a new game name.\n" + help();
@@ -165,21 +165,57 @@ public class ChessClient {
     private String drawBoard(ChessGame.TeamColor perspective, ChessBoard board) {
         String drawnBoard = "";
         if (perspective == ChessGame.TeamColor.BLACK) {
+            drawnBoard += blackColumnLabel();
             for (int row = 1; row <= 8; row++) {
+                drawnBoard += EscapeSequences.SET_BG_COLOR_MAGENTA + EscapeSequences.SET_TEXT_COLOR_BLACK + " " + row + " ";
                 for (int col = 8; col >= 1; col--) {
                     drawnBoard += makeSquare(board, row, col);
                 }
-                drawnBoard += "\n";
+                drawnBoard += EscapeSequences.SET_BG_COLOR_MAGENTA + EscapeSequences.SET_TEXT_COLOR_BLACK + " " + row + " ";
+                drawnBoard += EscapeSequences.RESET_BG_COLOR + EscapeSequences.RESET_TEXT_COLOR + "\n";
             }
+            drawnBoard += blackColumnLabel();
         } else {
+            drawnBoard += whiteColumnLabel();
             for (int row = 8; row >= 1; row--) {
+                drawnBoard += EscapeSequences.SET_BG_COLOR_MAGENTA + EscapeSequences.SET_TEXT_COLOR_BLACK + " " + row + " ";
                 for (int col = 1; col <= 8; col++) {
                     drawnBoard += makeSquare(board, row, col);
                 }
-                drawnBoard += "\n";
+                drawnBoard += EscapeSequences.SET_BG_COLOR_MAGENTA + EscapeSequences.SET_TEXT_COLOR_BLACK + " " + row + " ";
+                drawnBoard += EscapeSequences.RESET_BG_COLOR + EscapeSequences.RESET_TEXT_COLOR + "\n";
             }
+            drawnBoard += whiteColumnLabel();
         }
         return drawnBoard;
+    }
+
+    private String blackColumnLabel() {
+        String label = EscapeSequences.SET_BG_COLOR_MAGENTA + EscapeSequences.SET_TEXT_COLOR_BLACK + "    ";
+        label += "h" + " \u2003";
+        label += "g" + " \u2003";
+        label += "f" + " \u2003";
+        label += "e" + " \u2003";
+        label += "d" + " \u2003";
+        label += "c" + " \u2003";
+        label += "b" + " \u2003";
+        label += "a" + " \u2003";
+        label += "  " + EscapeSequences.RESET_BG_COLOR  + EscapeSequences.RESET_TEXT_COLOR + "\n";
+        return label;
+    }
+
+    private String whiteColumnLabel() {
+        String label = EscapeSequences.SET_BG_COLOR_MAGENTA + EscapeSequences.SET_TEXT_COLOR_BLACK + "    ";
+        label += "a" + " \u2003";
+        label += "b" + " \u2003";
+        label += "c" + " \u2003";
+        label += "d" + " \u2003";
+        label += "e" + " \u2003";
+        label += "f" + " \u2003";
+        label += "g" + " \u2003";
+        label += "h" + " \u2003";
+        label += "  " + EscapeSequences.RESET_BG_COLOR  + EscapeSequences.RESET_TEXT_COLOR + "\n";
+        return label;
     }
 
     private String makeSquare(ChessBoard board, int row, int col) {
