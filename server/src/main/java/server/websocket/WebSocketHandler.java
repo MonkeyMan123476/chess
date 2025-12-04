3635eabbb877ffc82148316e1b0097f1582c67eb
MonkeyMan123476 <csandorf@byu.edu>
2025-12-03 23:36:02 -0700

    Implemented resign mostly, fixed a bug with observers resigning.

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -131,6 +131,7 @@ public class WebSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
         if (!isPlayer) {
             session.getRemote().sendString(new Gson().toJson(new ErrorMessage("Only players can resign")));
             return;
+            // push
         }
 
         ChessGame game = gameData.game();
